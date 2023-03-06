6a044937d6434987beb9be769de0216c1af8dd31
somu3341 <somashekarvsv1996@gmail.com>
2023-03-06 13:29:22 +0530

    [somu]Add:Calculating Length of the Line

---
+++ LineComparison/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LineComparison
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine(" Enter x1");
+            int x1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2");
+            int x2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1");
+            int y1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2");
+            int y2= Convert.ToInt32(Console.ReadLine());
+            CalculateLength calculateLength=new CalculateLength(x1,x2, y1,y2);
+            calculateLength.Calculate();
+        }
+    }
+}
